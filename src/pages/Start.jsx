067b9d26a1c35e1e13a1377bf4a309cb70ab6dc3
juheesvt <wnglk62@gmail.com>
2024-02-10 20:48:42 +0900

    style: :recycle: prettier 코드 포맷팅 적용

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,34 +1,28 @@
-import { Container, Button, Image, Text } from "@chakra-ui/react";
+import { Container, Button, Image, Text } from '@chakra-ui/react';
 
-export const Start = ({nextPage, style}) => {
+export const Start = ({ nextPage, style }) => {
+  return (
+    <Container>
+      <Container style={style.headerStyle}>
+        <Text>mongle.</Text>
+      </Container>
 
-	return(
-		<Container>
-			<Container	
-				style={style.headerStyle}
-			>
-				<Text>mongle.</Text>
-			</Container>
-
-			<Container style={style.containerStyle}>
-				<Text style={style.subTitleStyle}>
-					나만의 대화메이트를 찾아볼까요?
-				</Text>
-					<Text style={style.titleStyle}>
-            몽글몽글 피어나는<br/>대화의 즐거움 💭
-          </Text>
-				<Container>
-					<Image src="start.svg" />
-				</Container>
-			</Container>
-			<Container>
-				<Button
-					style={style.buttonStyle}
-					onClick={nextPage}
-				>
-					초대코드 입력
-				</Button>
-			</Container>
-		</Container>  
-	);
-}
+      <Container style={style.containerStyle}>
+        <Text style={style.subTitleStyle}>나만의 대화메이트를 찾아볼까요?</Text>
+        <Text style={style.titleStyle}>
+          몽글몽글 피어나는
+          <br />
+          대화의 즐거움 💭
+        </Text>
+        <Container>
+          <Image src="start.svg" />
+        </Container>
+      </Container>
+      <Container>
+        <Button style={style.buttonStyle} onClick={nextPage}>
+          초대코드 입력
+        </Button>
+      </Container>
+    </Container>
+  );
+};
