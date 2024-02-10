1039e3a58e5bd19baf9030d337201e6d62759bf0
juheesvt <wnglk62@gmail.com>
2024-02-11 03:51:39 +0900

    feat: :sparkles: 이용 수칙 페이지 내부 코드 스타일 수정

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -1,77 +1,36 @@
-import { Button, Container, Text } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AppContextProvider, useAppContext } from "../context/appContext";
+import { Button, Container, Text } from '@chakra-ui/react';
 
-
-export const Rules = () => {
-
-	const globalStyle = {
-    maxWidth: "400px",
-    marginLeft: "auto",
-    marginRight: "auto",
-    backgroundColor: "#F9F9F7",
-    height: "100vh",
-    overflow: "hidden",
-    touchAction: "none",
-  };
-
-	return(
-		<Container style={globalStyle}>
-			<Container
-				textAlign="center"
-				fontFamily="BlackHanSans"
-				fontWeight="900"
-				fontSize="21"
-				marginBottom="20%"
-			>
-				<Text>mongle.</Text>
-			</Container>
-
-			<Container
-				paddingLeft="15%"
-			>
-				<Text
-          color="#868686"
-          fontSize="16"
-					fontWeight='300'
-				>
-					매너있는 대화를 위한,
-				</Text>
-				<Container
-					fontSize='25'
-					fontWeight='500'
-          marginBottom="30%"
-				>
-					<Text
-            margin="0"
-          >
-            몽글 이용 가이드 💭
-          </Text>
-				</Container>
-        <Container
-          marginBottom="70%"
-        >
-          <Text>1. <strong>대화 시간</strong>을 잘 지켜주세요.</Text>
-          <Text>2. 불쾌감을 줄 수 있는 말들은 <strong>조심</strong>!</Text>
-          <Text>3. 연락처와 실명 등 <strong>개인정보</strong>는 묻지 않아요.</Text>
-          <Text>4. 서로를 존중하는 <u>대화 매너</u>를 지켜주세요.</Text>
+export const Rules = ({ nextPage, style }) => {
+  return (
+    <Container>
+      <Container style={style.headerStyle}>
+        <Text>mongle.</Text>
       </Container>
-			</Container>
-      
-			<Container textAlign="center">
-				<Button
-					backgroundColor="black"
-					color="white"
-					size="lg"
-					padding="10"
-					width="350px"
-					borderRadius="23px"
-				>
-					입장하기
-				</Button>
-			</Container>
-		</Container>
-        
-	);
-}
+
+      <Container style={style.containerStyle}>
+        <Text style={style.subTitleStyle}>매너있는 대화를 위한,</Text>
+        <Text style={style.titleStyle}>몽글 이용 가이드 💭</Text>
+        <Container paddingLeft="0">
+          <Text marginBottom="2%">
+            1. <strong>대화 시간</strong>을 잘 지켜주세요.
+          </Text>
+          <Text marginBottom="2%">
+            2. 불쾌감을 줄 수 있는 말들은 <strong>조심</strong>!
+          </Text>
+          <Text marginBottom="2%">
+            3. 연락처와 실명 등 <strong>개인정보</strong>는 묻지 않아요.
+          </Text>
+          <Text marginBottom="2%">
+            4. 서로를 존중하는 <u>대화 매너</u>를 지켜주세요.
+          </Text>
+        </Container>
+      </Container>
+
+      <Container>
+        <Button style={style.buttonStyle} onClick={nextPage}>
+          초대코드 입력
+        </Button>
+      </Container>
+    </Container>
+  );
+};
