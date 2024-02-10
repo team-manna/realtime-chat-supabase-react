99f8e2d31968b2a31915f4facd7f27e9baa6f68c
juheesvt <wnglk62@gmail.com>
2024-02-10 20:17:15 +0900

    style: :recycle: 페이지 별로 공통된 컴포넌트 스타일 props로 내림

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,48 +1,29 @@
 import { Container, Button, Image, Text } from "@chakra-ui/react";
 
-export const Start = ({nextPage, buttonStyle}) => {
+export const Start = ({nextPage, style}) => {
 
 	return(
 		<Container>
-			<Container
-				textAlign="center"
-				fontFamily="BlackHanSans"
-				fontWeight="900"
-				fontSize="21"
-				marginBottom="15%"
-				marginTop="5%"
+			<Container	
+				style={style.headerStyle}
 			>
 				<Text>mongle.</Text>
 			</Container>
 
-			<Container
-				paddingLeft="15%"
-				height="70vh"
-			>
-				<Text
-					color="#868686"
-					fontWeight='300'
-					marginBottom="2%"
-				>
+			<Container style={style.containerStyle}>
+				<Text style={style.subTitleStyle}>
 					나만의 대화메이트를 찾아볼까요?
 				</Text>
-				<Container
-					fontWeight='500'
-					fontSize='28'
-					padding="0"
-					marginBottom="20%"
-				>
-					<Text>몽글몽글 피어나는<br/>대화의 즐거움 💭</Text>
-				</Container>
-				<Container 
-					textAlign="center"
-				>
+					<Text style={style.titleStyle}>
+            몽글몽글 피어나는<br/>대화의 즐거움 💭
+          </Text>
+				<Container>
 					<Image src="start.svg" />
 				</Container>
 			</Container>
 			<Container>
 				<Button
-					style={buttonStyle}
+					style={style.buttonStyle}
 					onClick={nextPage}
 				>
 					초대코드 입력
