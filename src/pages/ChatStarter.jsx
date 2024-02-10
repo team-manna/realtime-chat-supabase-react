39498f15c36ffb5887601015694d546baeda392d
juheesvt <wnglk62@gmail.com>
2024-02-10 21:57:37 +0900

    feat: :sparkles: 채팅방 입장 전까지의 페이지들을 묶는 컴포넌트

---
+++ src/pages/ChatStarter.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { Container } from '@chakra-ui/react';
+import { InvitationCode } from './InvitationCode';
+import { Start } from './Start';
+import { Rules } from './Rules';
+import { End } from './End';
+
+export const ChatStarter = () => {
+  const [currentPage, setCurrentPage] = useState('start');
+  const style = {
+    buttonStyle: {
+      backgroundColor: '#1B1B1B',
+      borderRadius: '23px',
+      width: '100%',
+      color: '#FFFFFF',
+      fontSize: 'medium',
+      fontWeight: '600',
+    },
+    headerStyle: {
+      textAlign: 'center',
+      fontWeight: '400',
+      fontSize: '21',
+      marginBottom: '15%',
+      marginTop: '5%',
+    },
+    subTitleStyle: {
+      color: '#868686',
+      fontWeight: '500',
+      fontSize: '15px',
+      marginBottom: '2%',
+    },
+    titleStyle: {
+      color: '#000000',
+      fontWeight: '700',
+      fontSize: '28px',
+      padding: '0',
+      marginBottom: '40%',
+    },
+    containerStyle: {
+      paddingLeft: '15%',
+      height: '80vh',
+    },
+  };
+
+  const nextPage = () => {
+    switch (currentPage) {
+      case 'start':
+        setCurrentPage('invitation');
+        break;
+      case 'invitation':
+        setCurrentPage('rules');
+        break;
+      case 'rules':
+        setCurrentPage('end');
+        break;
+      default:
+        setCurrentPage('start');
+    }
+  };
+  return (
+    <Container>
+      {currentPage === 'start' && <Start nextPage={nextPage} style={style} />}
+      {currentPage === 'invitation' && (
+        <InvitationCode nextPage={nextPage} style={style} />
+      )}
+      {currentPage === 'rules' && <Rules nextPage={nextPage} />}
+      {currentPage === 'end' && <End />}
+    </Container>
+  );
+};
